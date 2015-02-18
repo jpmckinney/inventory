ea6eda74b41cba5d695bea7ae1eb5801c968e755
James McKinney <james@slashpoundbang.com>
2015-02-18 01:51:20 -0500

    process 100 simultaneous requests at a time

--- inventory/management/commands/headers.py
+++ inventory/management/commands/headers.py
@@ -24,60 +24,71 @@ class Command(InventoryCommand):
 
         session = FuturesSession()
 
+        # Collect the distribution-response pairs.
+        def callback(distribution, response):
+            results.append([distribution, response])
+
+        # Create a closure.
+        def factory(distribution):
+            return lambda session, response: callback(distribution, response)
+
         for catalog in self.catalogs:
-            distributions = Distribution.objects.filter(division_id=catalog.division_id, http_content_length__isnull=True)
+            distributions = Distribution.objects.filter(division_id=catalog.division_id, http_status_code__isnull=True)
 
             if options['media_type']:
                 distributions = distributions.filter(mediaType=options['media_type'])
 
-            # @see https://github.com/ross/requests-futures/issues/18
-            # @see https://github.com/ross/requests-futures/issues/5
-            futures = []
-            results = []
+            if not distributions.exists():
+                continue
 
-            # Collect the distribution-response pairs.
-            def callback(distribution, response):
-                results.append([distribution, response])
+            # @see https://djangosnippets.org/snippets/1949/
+            pk = 0
+            last_pk = distributions.order_by('-pk')[0].pk
+            distributions = distributions.order_by('pk')
+            while pk < last_pk:
+                # @see https://github.com/ross/requests-futures/issues/18
+                # @see https://github.com/ross/requests-futures/issues/5
+                futures = []
+                results = []
 
-            # Create a closure.
-            def factory(distribution):
-                return lambda session, response: callback(distribution, response)
+                for distribution in distributions.filter(pk__gt=pk)[:100]:
+                    pk = distribution.pk
 
-            for distribution in distributions:
-                # @see http://docs.python-requests.org/en/latest/user/advanced/#body-content-workflow
-                # @see http://stackoverflow.com/a/845595/244258
-                futures.append(session.get(quote(distribution.accessURL, safe="%/:=&?~#+!$,;'@()*[]"),
-                    stream=True,
-                    allow_redirects=False,
-                    background_callback=factory(distribution)))
+                    # @see http://docs.python-requests.org/en/latest/user/advanced/#body-content-workflow
+                    # @see http://stackoverflow.com/a/845595/244258
+                    futures.append(session.get(quote(distribution.accessURL, safe="%/:=&?~#+!$,;'@()*[]"),
+                        stream=True,
+                        allow_redirects=False,
+                        background_callback=factory(distribution)))
 
-            for future in futures:
-                try:
-                    future.result()
-                except requests.packages.urllib3.exceptions.ProtocolError:
-                    self.exception('')
-                except requests.exceptions.InvalidURL:
-                    self.exception('')
+                for future in futures:
+                    try:
+                        future.result()
+                    except (requests.exceptions.InvalidSchema, requests.exceptions.InvalidURL, requests.exceptions.SSLError, requests.packages.urllib3.exceptions.ProtocolError):
+                        self.exception('')
 
-            for distribution, response in results:
-                status_code = response.status_code
-                content_length = response.headers.get('content-length')
-                charset = ''
+                for distribution, response in results:
+                    status_code = response.status_code
+                    charset = ''
 
-                # @see https://github.com/kennethreitz/requests/blob/b137472936cbe6a6acabab538c1d05ed4c7da638/requests/utils.py#L308
-                content_type = response.headers.get('content-type', '')
-                if content_type:
-                    content_type, params = cgi.parse_header(content_type)
-                    if 'charset' in params:
-                        charset = params['charset'].strip("'\"")
+                    content_length = response.headers.get('content-length')
+                    if content_length:
+                        content_length = int(content_length)
 
-                distribution.http_headers = dict(response.headers)
-                distribution.http_status_code = status_code
-                distribution.http_content_length = content_length
-                distribution.http_content_type = content_type
-                distribution.http_charset = charset
-                distribution.save()
+                    # @see https://github.com/kennethreitz/requests/blob/b137472936cbe6a6acabab538c1d05ed4c7da638/requests/utils.py#L308
+                    content_type = response.headers.get('content-type', '')
+                    if content_type:
+                        content_type, params = cgi.parse_header(content_type)
+                        if 'charset' in params:
+                            charset = params['charset'].strip("'\"")
 
-                self.debug('{} {} {}'.format(status_code, content_length and number_to_human_size(int(content_length)), content_type))
+                    distribution.http_headers = dict(response.headers)
+                    distribution.http_status_code = status_code
+                    distribution.http_content_length = content_length
+                    distribution.http_content_type = content_type
+                    distribution.http_charset = charset
+                    distribution.save()
 
-                response.close()
+                    self.debug('{} {} {}'.format(status_code, number_to_human_size(content_length), content_type))
+
+                    response.close()
