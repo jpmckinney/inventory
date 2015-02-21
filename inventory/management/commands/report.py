51d9745ddedfefb223339f23e351e549031d627c
James McKinney <james@slashpoundbang.com>
2015-02-21 10:20:31 -0500

    add analysis of domain names for direct download access method

--- inventory/management/commands/report.py
+++ inventory/management/commands/report.py
@@ -2,6 +2,7 @@ import json
 import sys
 from collections import defaultdict
 from optparse import make_option
+from urllib.parse import urlparse
 
 import ckanapi
 import pandas as pd
@@ -16,6 +17,8 @@ class Command(InventoryCommand):
     help = 'Analyzes catalogs'
 
     option_list = InventoryCommand.option_list + (
+        make_option('--access', action='append_const', dest='reports', const='access',
+                    help='Classification of direct download domain names, for import into R.'),
         make_option('--dcat', action='append_const', dest='reports', const='dcat',
                     help='Usage of DCAT by CKAN catalogs.'),
         make_option('--pod', action='append_const', dest='reports', const='pod',
@@ -24,8 +27,6 @@ class Command(InventoryCommand):
                     help='Usage of Schema.org.'),
         make_option('--federation', action='append_const', dest='reports', const='federation',
                     help='Usage of Federation technologies.'),
-        make_option('--media-types', action='append_const', dest='reports', const='media_types',
-                    help='Usage of media types.'),
         make_option('--licenses', action='append_const', dest='reports', const='licenses',
                     help='Usage of licenses.'),
         make_option('--csv', action='store_const', dest='format', const='csv',
@@ -81,6 +82,24 @@ class Command(InventoryCommand):
 
         return self.series(getter)
 
+    def access(self):
+        frame = defaultdict(lambda: defaultdict(int))
+        for catalog in self.catalogs:
+            urls = Distribution.objects.filter(division_id=catalog.division_id).values_list('accessURL', flat=True)
+            if urls:
+                frame['count'][catalog.division_id] = urls.count()
+                for url in urls:
+                    host = urlparse(url).netloc.split(':', 1)[0]
+                    if host.endswith(catalog_domains[catalog.division_id]):
+                        key = 'Catalog'
+                    elif any(host.endswith(suffix) for suffix in government_domains[catalog.division_id]):
+                        key = 'Government'
+                    else:
+                        key = 'Other'
+                        # frame[host][catalog.division_id] += 1
+                    frame[key][catalog.division_id] += 1
+        return pd.DataFrame(frame)
+
     def federation(self):
         frame = defaultdict(lambda: defaultdict(int))
         for catalog in self.catalogs:
@@ -115,9 +134,6 @@ class Command(InventoryCommand):
 
     def licenses(self):
         self.report(Dataset, 'license', distinct='id')
-
-    def media_types(self):
-        self.report(Distribution, 'mediaType', distinct='dataset_id')
 
     def report(self, klass, field, *, distinct):
         for catalog in self.catalogs:
@@ -221,4 +237,183 @@ source_types = {
 validators = {
     'fgdc_minimal': 'fgdc',
     'iso19139ngdc': 'iso19139',
+}
+
+catalog_domains = {
+    'ocd-division/country:ar': 'datospublicos.gob.ar',
+    'ocd-division/country:au': 'data.gov.au',
+    'ocd-division/country:br': 'dados.gov.br',
+    'ocd-division/country:ca': 'data.gc.ca',
+    'ocd-division/country:cl': 'datos.gob.cl',
+    'ocd-division/country:cr': 'gobiernodigitalcr.cloudapi.junar.com',
+    'ocd-division/country:ee': 'opendata.riik.ee',
+    'ocd-division/country:es': 'datos.gob.es',
+    'ocd-division/country:fi': 'avoindata.fi',
+    'ocd-division/country:fr': 'data.gouv.fr',
+    'ocd-division/country:gb': 'data.gov.uk',
+    'ocd-division/country:gh': 'data.gov.gh',
+    'ocd-division/country:gr': 'data.gov.gr',
+    'ocd-division/country:id': 'data.id',
+    'ocd-division/country:ie': 'data.gov.ie',
+    'ocd-division/country:it': 'dati.gov.it',
+    'ocd-division/country:ke': 'opendata.go.ke',
+    'ocd-division/country:md': 'data.gov.md',
+    'ocd-division/country:mx': 'datos.gob.mx',
+    'ocd-division/country:nl': 'data.overheid.nl',
+    'ocd-division/country:ph': 'data.gov.ph',
+    'ocd-division/country:py': 'datos.gov.py',
+    'ocd-division/country:ro': 'data.gov.ro',
+    'ocd-division/country:se': 'oppnadata.se',
+    'ocd-division/country:sk': 'data.gov.sk',
+    'ocd-division/country:tz': 'opendata.go.tz',
+    'ocd-division/country:us': 'data.gov',
+    'ocd-division/country:uy': 'catalogodatos.gub.uy',
+}
+
+# We only categorize the most popular domains, i.e. >=0.1% of distributions.
+government_domains = {
+    'ocd-division/country:ar': ['.gob.ar'],
+    'ocd-division/country:au': ['.gov.au'],
+    'ocd-division/country:br': [
+        '.gov.br',
+        '.jus.br',
+        '.leg.br',
+    ],
+    'ocd-division/country:ca': [
+        '.gc.ca',
+        'geobase.ca',  # Natural Resources Canada
+    ],
+    'ocd-division/country:cl': ['.gob.cl'],
+    'ocd-division/country:cr': ['.go.cr'],
+    'ocd-division/country:ee': [
+        '.riik.ee',  # "state"
+        '.muinas.ee',  # http://et.wikipedia.org/wiki/Muinsuskaitseamet
+    ],
+    'ocd-division/country:es': [
+        '.gob.es',
+        '.cnig.es',  # Centro Nacional de Información Geográfica
+        '.chj.es',  #  Confederación Hidrográfica del Júcar
+        '.agenciatributaria.es',  # Agencia Tributaria
+        '.fega.es',  # Fondo Español de Garantía Agraria
+        '.ign.es',  #  Instituto Geográfico Nacional
+        '.imserso.es',  # Instituto de Mayores y Servicios Sociales
+        '.ine.es',  # Instituto Nacional de Estadística
+        '.ipyme.org',  # Dirección General de Industria y de la Pequeña y Mediana Empresa
+        '.cis.es',  # Centro de investigaciones sociológicas
+    ],
+    'ocd-division/country:fi': [  # 0.005
+        '.gtk.fi',  # Geological Survey of Finland
+        '.liikennevirasto.fi',  # Finnish Transport Agency
+        '.maanmittauslaitos.fi',  # National Land Survey
+        '.paikkatietoikkuna.fi',  # National Land Survey
+        '.sfs.fi',  # Finnish Standards Association
+        '.stat.fi',  # Statistics Finland
+        '.vahtiohje.fi',  # Ministry of Finance
+    ],
+    'ocd-division/country:fr': ['.gouv.fr'],
+    'ocd-division/country:gb': [
+        '.gov.uk',
+        '.hpa.org.uk',  # Public Health England
+        '.isdscotland.org',  # Scottish Government
+        '.nhs.uk',  # Department of Health
+        '.ordnancesurvey.co.uk',  # Department for Business, Innovation and Skills
+        '.slc.co.uk',  # http://en.wikipedia.org/wiki/Student_Loans_Company
+        '.uktradeinfo.com',  # HM Revenue and Customs
+        'opendatacommunities.org',  # Department for Communities and Local Government
+    ],
+    'ocd-division/country:gh': ['.gov.gh'],
+    'ocd-division/country:gr': [  # 0.005
+        '.gov.gr',
+        '.astynomia.gr',  # http://en.wikipedia.org/wiki/Hellenic_Police
+        '.ekdd.gr',  # http://en.wikipedia.org/wiki/National_Centre_for_Public_Administration_and_Local_Government
+    ],
+    'ocd-division/country:id': ['.go.id'],
+    'ocd-division/country:ie': [  # 0.005
+        '.gov.ie',
+        '.marine.ie',  # Marine Institute
+        '.cso.ie',  # Central Statistics Office Ireland
+        '.hea.ie',  # Higher Education Authority
+        '.buildingsofireland.ie',  # Department of Arts, Heritage and the Gaeltacht
+        '.epa.ie',  # Environmental Protection Agency
+        '.education.ie',  # Department of Education and Skills
+        '.environ.ie',  # Department of the Environment, Community and Local Government
+        '.fishingnet.ie',  # Department of Agriculture, Food and the Marine
+        '.gsi.ie',  # Geological Survey of Ireland
+        '.infomar.ie',  # Geological Survey of Ireland / Marine Institute
+        '.seai.ie',  # Sustainable Energy Authority
+    ],
+    'ocd-division/country:it': [
+        '.gov.it',
+        '.inail.it',  # Istituto Nazionale Assicurazione contro gli Infortuni sul Lavoro
+        '.inps.it',  # Istituto Nazionale Previdenza Sociale
+        '.istat.it',  # Istituto nazionale di statistica
+        '.politicheagricole.it',  # Ministero delle politiche agricole alimentari e forestali
+    ],
+    'ocd-division/country:ke': [
+        '.go.ke',
+        '.kenyalaw.org',  # National Council for Law Reporting
+    ],
+    'ocd-division/country:md': [
+        '.gov.md',
+        '.statistica.md',  # Biroul Naţional de Statistică al Republicii Moldova
+        '.knbs.or.ke',  # Kenya National Bureau of Statistics
+    ],
+    'ocd-division/country:mx': [
+        '.gob.mx',
+        '.nafin.com',  # Nacional Financiera, state bank
+        '.pemex.com',  # Petróleos Mexicanos, state-owned
+    ],
+    'ocd-division/country:nl': [
+        '.duo.nl',  # Ministerie van Onderwijs, Cultuur en Wetenschap
+        '.gdngeoservices.nl',  # Geologische Dienst Nederland
+        '.kaartenbalie.nl',  # Ministerie van Economische Zaken
+        '.kadaster.nl',  # Cadastre, Land Registry and Mapping Agency
+        '.knmi.nl',  # Ministry of Infrastructure and the Environment
+        '.nationaalgeoregister.nl',
+        '.overheid.nl', # "government"
+        '.pbl.nl',  # Planbureau voor de Leefomgeving
+        '.rijkswaterstaat.nl',  # Ministerie van Infrastructuur en Milieu
+        '.risicokaart.nl',
+        '.rivm.nl',  # Ministerie van Volksgezondheid, Welzijn en Sport
+        '.rwsgeoweb.nl',
+        '.waterschapservices.nl',
+    ],
+    'ocd-division/country:ph': ['.gov.ph'],
+    'ocd-division/country:py': ['.gov.py'],
+    'ocd-division/country:ro': [  # 0.005
+        '.gov.ro',
+        '.edu.ro',  # Ministerul Educației Naționale
+        '.inforegio.ro',  # Ministry of Regional Development and Public Administration
+        '.mfinante.ro',  # Ministerul Finanţelor Publice
+        '.mmuncii.ro',  # Ministerul Muncii, Familiei, Protecţiei Sociale şi Persoanelor Vârstnice
+    ],
+    'ocd-division/country:se': [  # 0.025
+        '.gov.se',
+        '.sgu.se',  # http://en.wikipedia.org/wiki/Geological_Survey_of_Sweden
+        '.socialstyrelsen.se',  # http://en.wikipedia.org/wiki/National_Board_of_Health_and_Welfare_%28Sweden%29
+        '.verksamt.se',
+    ],
+    'ocd-division/country:sk': [  # 0.005
+        '.gov.sk',
+        '.justice.sk',  # Ministerstvo Spravodlivosti
+        '.mfa.sk',  # Ministry of Foreign and European Affairs of the Slovak Republic
+        '.mhsr.sk',  # Ministry of Economy of the Slovak Republic
+        '.minv.sk',  # Ministry of Interior of the Slovak Republic
+        '.mosr.sk',  # Ministry of Defense of Slovak Republic
+        '.mpsr.sk',  # Ministry of Agriculture and Rural Development of the Slovak Republic
+        '.nrsr.sk',  # National Council of the Slovak Republic
+        '.sazp.sk',  #  Slovenská agentúra životného prostredia
+        '.skgeodesy.sk',  # Geodesy, Cartography and Cadastre Authority of Slovak Republic
+        '.statistics.sk',  # Statistical Office of the Slovak Republic
+        '.svssr.sk',  # State Veterinary and Food Administration of the Slovak Republic
+    ],
+    'ocd-division/country:tz': ['.go.tz'],
+    'ocd-division/country:us': [
+        '.gov',
+        '.mil',
+    ],
+    'ocd-division/country:uy': [
+        '.gub.uy',
+        '.precios.uy',  #  Ministerio de Economía y Finanzas
+    ],
 }
--- r/structure.R
+++ r/structure.R
@@ -1,5 +1,6 @@
 # You may need to `setwd('~/path/to/inventory/r')`
 library(ggplot2)
+library(reshape2)
 source('includes/constants.R')
 source('includes/human.R')
 source('includes/database.R')
@@ -27,3 +28,41 @@ ggplot(data=rows, aes(x=division_id, y=count)) + geom_boxplot(outlier.size=0) + 
 mean(rows$count)
 
 sd(rows$count)
+
+
+
+# Get the per-catalog classifications of access URL domain names.
+rows <- read.csv('~/path/to/inventory/r/access.csv', header=TRUE)
+colnames(rows)[1] <- 'division_id'
+rows <- rows[!rows$division_id %in% subnational,]
+
+rows[2:4] <- rows[2:4] / rows$count
+rows$count <- NULL
+
+m <- melt(rows, 'division_id')
+m$division_id <- human_division_ids(m$division_id)
+
+# Unused as too many catalogs contain subnational data.
+ggplot(data=m, aes(x=division_id, y=value, fill=variable)) + geom_bar(stat='identity') + theme(
+  axis.title=element_blank(),
+  text=element_text(size=16)
+) + coord_flip()
+
+
+
+# Identify domains to add to the bottom of report.py.
+rows <- read.csv('~/path/to/inventory/r/domains.csv', header=TRUE)
+colnames(rows)[1] <- 'division_id'
+rows <- rows[!rows$division_id %in% subnational,]
+
+count <- rows$count
+names(count) <- rows$division_id
+rows$count <- NULL
+
+m <- melt(rows, 'division_id')
+
+summarize <- function (country_code, threshold=0.001) {
+  division_id <- paste('ocd-division/country:', country_code, sep='')
+  r <- na.omit(m[m$division_id==division_id,])
+  return(r[r$value>=count[division_id]*threshold,])
+}
